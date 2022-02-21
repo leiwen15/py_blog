290b44e096c06a25ff22d66c581c35dfaf93f256
leiwen15 <41476862+leiwen15@users.noreply.github.com>
2022-02-21 23:04:17 +0800

    modify Chapter 15

--- app/email.py
+++ app/email.py
@@ -1,7 +1,6 @@
 from threading import Thread
-from flask import render_template
+from flask import current_app
 from flask_mail import Message
-from flask_babel import _
 from app import mail
 
 
@@ -16,6 +15,3 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg.html = html_body
     Thread(target=send_async_email,
            args=(current_app._get_current_object(), msg)).start()
-
-
-
--- app/main/routes.py
+++ app/main/routes.py
@@ -3,11 +3,9 @@ from flask import render_template, flash, redirect, url_for, request, g, \
     jsonify, current_app
 from flask_login import current_user, login_required
 from flask_babel import _, get_locale
-from langdetect import detect, LangDetectException
 from app import db
 from app.main.forms import EditProfileForm, EmptyForm, PostForm
 from app.models import User, Post
-from app.translate import translate
 from app.main import bp
 
 @bp.before_request
