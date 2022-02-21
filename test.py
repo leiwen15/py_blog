ee54698f9ec38b8ba8590eda3c872dd0fd68075a
leiwen15 <41476862+leiwen15@users.noreply.github.com>
2022-02-21 08:08:47 +0800

    chapter 15

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask_mail import Message
-from app import mail, app
-# import app
-
-print(app.config['ADMINS'][0])
-msg = Message('test subject', sender=app.config['ADMINS'][0], recipients=['1141677899@qq.com'])
-msg.body = 'text body'
-msg.html = '<h1>HTML body</h1>'
-mail.send(msg)
